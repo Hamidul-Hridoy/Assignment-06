339445366ea7270b1097074849292c3223989594
Hamidul-Hridoy <hamidulhridoy05@gmail.com>
2025-09-23 10:21:03 +0600

    fifth commit

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ const loadcate = () => {
     .then((cate) => displaycate(cate.categories));
 }
 
+
+
 const displaycate = (categorie) => {
     const cateContainer = document.getElementById("cate-container");
     cateContainer.innerHTML = "";
     console.log(categorie);
 
+    
+
     for(let part of categorie){
         const div1 = document.createElement("div");
         div1.innerHTML =`
-        <button class="w-[250px] h-[40px] text-left focus:h-[35px] pl-[10px] focus:bg-[#15803D] focus:text-white text-[16px] ">${part.category_name} </button>
+        <button onclick="types(${part.id})" class="w-[250px] h-[40px] text-left focus:h-[35px] pl-[10px] focus:bg-[#15803D] focus:text-white text-[16px] ">${part.category_name} </button>
         `
         cateContainer.append(div1);
     }
@@ -20,11 +24,12 @@ const displaycate = (categorie) => {
 loadcate();
 
 
+
 const loadAll = () => {
     fetch("https://openapi.programming-hero.com/api/plants")
     .then((response) => response.json())
     .then((allPlants) => displayAll(allPlants.plants));
-}
+};
 
 const displayAll = (trees) => {
     const allContainer = document.getElementById("all-container");
@@ -35,18 +40,97 @@ const displayAll = (trees) => {
         const div2 = document.createElement("div");
         div2.innerHTML =`
         <div class="w-[343.33px] h-auto bg-white p-[16px] rounded-[8px]">
-            <img src="${tree.image}" class="w-[311.33px] h-[186.8px] rounded-[8px]">
-            <h3 class="text-[14px] text-[#18181B] font-bold mt-[12px]">${tree.name}</h3>
+            <img src="${tree.image}" class="w-[311.33px] h-[186.8px] rounded-[8px] object-cover">
+
+            <a  href="#my_modal_8" class="btn text-[14px] text-[#18181B] font-bold mt-[12px] border-none bg-white">${tree.name}</a>
+
+
+            <div class="modal" role="dialog" id="my_modal_8">
+                <div class="modal-box gap-2">
+                    <h3 class="text-2xl font-bold">${tree.name}</h3>
+                    <img src="${tree.image}" class="w-[511.33px] h-[300.8px] rounded-[8px] object-cover">
+                    <h2 class="text-xl font-semibold">Category:<span class="text-[16px] font-medium opacity-70"> ${tree.category}</span></h2>
+                    <h2 class="text-xl font-semibold">Price:<span class="text-[16px] font-medium opacity-70"> ${tree.price}</span></h2>
+                    <h2 class="text-xl font-semibold">Description:<span class="text-[16px] font-medium opacity-70"> ${tree.description}</span></h2>
+                    <p class="py-4">This modal works with anchor links</p>
+                <div class="modal-action">
+                <a href="#" class="btn">Close</a>
+                </div>
+            </div>
+        </div>
+
             <p class="text-[12px] text-[#71717A] opacity-80 mt-[8px]">${tree.description}</p>
             <div class="flex justify-between mt-[8px]">
                 <button class="w-auto px-[12px] h-[28px] rounded-[400px] bg-[#DCFCE7] text-[#15803D] text-[14px] font-semibold geist">${tree.category}</button>
                 <h2 class="text-[14px] font-bold text-[#1F2937]"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${tree.price}</h2>
             </div>
-            <button class="w-[311.33px] h-[43px] text-center focus:h-[35px] pl-[10px] bg-[#15803D] font-semibold text-white text-[16px] rounded-[999px] px-[20px] mt-[12px]">Add to Cart </button>
+            <button id="cartBtn" class="w-[311.33px] h-[43px] text-center focus:h-[35px] pl-[10px] bg-[#15803D] font-semibold text-white text-[16px] rounded-[999px] px-[20px] mt-[12px]">Add to Cart</button>
+
         </div>
-        
         `
         allContainer.append(div2);
     }
+
+
+    
+        const btn=document.getElementById("cartBtn");
+const list=document.getElementById("cart");
+list.innerHTML="";
+
+btn.addEventListener("click", () => {
+    list.innerHTML += ` 
+        <div class="w-[200px] h-auto pl=[50px]">
+            <h1>hello</h1></div>`;
+});
+    
+
+console.log(btn.innerText);
 }
 loadAll();
+
+const types = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then((res)=>res.json())
+    .then((type)=>ty_plants(type.plants));
+};
+
+const ty_plants = (elements) =>{
+    const typebox = document.getElementById("all-container");
+    typebox.innerHTML = "";
+
+    for(let element of elements){
+        const div3 = document.createElement("div");
+        div3.innerHTML =`
+        <div class="w-[343.33px] h-auto bg-white p-[16px] rounded-[8px]">
+            <img src="${element.image}" class="w-[311.33px] h-[186.8px] rounded-[8px] object-cover">
+
+            <a  href="#my_modal_8" class="btn text-[14px] text-[#18181B] font-bold mt-[12px] border-none bg-white">${element.name}</a>
+
+
+            <div class="modal" role="dialog" id="my_modal_8">
+                <div class="modal-box gap-2">
+                    <h3 class="text-2xl font-bold">${element.name}</h3>
+                    <img src="${element.image}" class="w-[511.33px] h-[300.8px] rounded-[8px] object-cover">
+                    <h2 class="text-xl font-semibold">Category:<span class="text-[16px] font-medium opacity-70"> ${element.category}</span></h2>
+                    <h2 class="text-xl font-semibold">Price:<span class="text-[16px] font-medium opacity-70"> ${element.price}</span></h2>
+                    <h2 class="text-xl font-semibold">Description:<span class="text-[16px] font-medium opacity-70"> ${element.description}</span></h2>
+                    <p class="py-4">This modal works with anchor links</p>
+                <div class="modal-action">
+                <a href="#" class="btn">Close</a>
+                </div>
+            </div>
+        </div>
+
+            <p class="text-[12px] text-[#71717A] opacity-80 mt-[8px]">${element.description}</p>
+            <div class="flex justify-between mt-[8px]">
+                <button class="w-auto px-[12px] h-[28px] rounded-[400px] bg-[#DCFCE7] text-[#15803D] text-[14px] font-semibold geist">${element.category}</button>
+                <h2 class="text-[14px] font-bold text-[#1F2937]"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${element.price}</h2>
+            </div>
+            <button id="cartBtn" class="w-[311.33px] h-[43px] text-center focus:h-[35px] pl-[10px] bg-[#15803D] font-semibold text-white text-[16px] rounded-[999px] px-[20px] mt-[12px]">Add to Cart</button>
+
+        </div>
+        `
+        typebox.append(div3);
+    };
+};
+loadtypes();
